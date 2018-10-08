c774817726a1cd58735f1c3f1a8b2072240c0a81
flythinker <yipenghuang@hotmail.com>
2018-10-08 18:53:37 +0800

    基本项目结构
    基本项目结构

---
+++ dev_test/test1.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/common.php';
+
+use flythinker\log2redis\Logger; 
+$logger = \flythinker\log2redis\Logger::getLogger("abc");
+
---
+++ src/flythinker/log2redis/Logger.php
@@ -0,0 +1,12 @@
+<?php
+namespace flythinker\log2redis;
+
+class Logger
+{
+    public static function getLogger($name)
+    {
+        echo "getLogger $name";
+    }
+}
+
+
